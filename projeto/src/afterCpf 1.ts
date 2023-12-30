f14f16bd507d95379ba38c5daef41f9ec1b08f34
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-12-30 15:08:21 -0300

    fix: Refatora código
    Remove identação desnecessária, renomeia variavel

--- projeto/src/afterCpf 1.ts
+++ projeto/src/afterCpf 1.ts
@@ -1,62 +1,51 @@
 // @ts-nocheck
-export function validateCpf(str) {
+export function validateCpf(cpf) {
+    if (cpf === null || cpf === undefined) return false
 
-    if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 || str.length <= 14) {
+    cpf = cpf.replace(/\D/g, '')
 
-                str = str
-                    .replace('.', '')
-                    .replace('.', '')
-                    .replace('-', '')
-                    .replace(" ", "");
+    if (cpf.length !== 11) return false
 
-                if (!str.split("").every(c => c === str[0])) {
-                    try {
-                        let d1, d2;
-                        let dg1, dg2, rest;
-                        let digito;
-                        let nDigResult;
-                        d1 = d2 = 0;
-                        dg1 = dg2 = rest = 0;
+    if (!cpf.split("").every(c => c === cpf[0])) {
+        try {
+            let d1, d2;
+            let dg1, dg2, rest;
+            let digito;
+            let nDigResult;
+            d1 = d2 = 0;
+            dg1 = dg2 = rest = 0;
 
-                        for (let nCount = 1; nCount < str.length - 1; nCount++) {
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
+            for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
+                // if (isNaN(parseInt(cpf.subcpfing(nCount -1, nCount)))) {
+                // 	return false;
+                // } else {
 
-                            digito = parseInt(str.substring(nCount - 1, nCount));
-                            d1 = d1 + (11 - nCount) * digito;
+                digito = parseInt(cpf.substring(nCount - 1, nCount));
+                d1 = d1 + (11 - nCount) * digito;
 
-                            d2 = d2 + (12 - nCount) * digito;
-                            // }
-                        }
-                        ;
+                d2 = d2 + (12 - nCount) * digito;
+                // }
+            }
+            ;
 
-                        rest = (d1 % 11);
+            rest = (d1 % 11);
 
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-                        d2 += 2 * dg1;
-                        rest = (d2 % 11);
-                        if (rest < 2)
-                            dg2 = 0;
-                        else
-                            dg2 = 11 - rest;
+            dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+            d2 += 2 * dg1;
+            rest = (d2 % 11);
+            if (rest < 2)
+                dg2 = 0;
+            else
+                dg2 = 11 - rest;
 
-                        let nDigVerific = str.substring(str.length - 2, str.length);
-                        nDigResult = "" + dg1 + "" + dg2;
-                        return nDigVerific == nDigResult;
-                    } catch (e) {
-                        console.error("Erro !" + e);
+            let nDigVerific = cpf.substring(cpf.length - 2, cpf.length);
+            nDigResult = "" + dg1 + "" + dg2;
+            return nDigVerific == nDigResult;
+        } catch (e) {
+            console.error("Erro !" + e);
 
-                        return false;
-                    }
-                } else return false
-
-            } else return false;
+            return false;
         }
-
-
-    } else return false;
+    } else return false
 
 }
